04b3637969aa9413066a7755cce26d38825829dd
Levi Korell <levi.korell1@gmail.com>
2026-09-13 15:33:06 -0700

    boxes set up, not yet functional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+console.log("Script is running");
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,15 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+/* 
+app.post("/submit", (req, res) => {
+  const numLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numberOfLetters: numLetters });
+});
+
+*/
+
+// the above is the default code taken from the numLetters exercise, establishing a connection & function for receiving and sending data
 
 app.post("/submit", (req, res) => {
   const numLetters = req.body["fName"].length + req.body["lName"].length;
@@ -23,3 +33,4 @@ app.post("/submit", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
