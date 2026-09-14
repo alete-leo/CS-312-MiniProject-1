0d50a6ba7f8c83171c4b503ab144c060dd8e2e37
Levi Korell <levi.korell1@gmail.com>
2026-09-13 17:14:59 -0700

    fixed locals issue, re-did structure from sample. Still no styling or edit functionality.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public")); // for the css
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 /* 
 app.post("/submit", (req, res) => {
   const numLetters = req.body["fName"].length + req.body["lName"].length;
+  // from <input type="text" name="fName" placeholder="Blog Title" /> , <% if (locals.numberOfLetters) { %> , <h1>There are <%= numberOfLetters %> blog posts here.</h1>
   res.render("index.ejs", { numberOfLetters: numLetters });
 });
 
@@ -24,13 +25,68 @@ app.post("/submit", (req, res) => {
 
 // the above is the default code taken from the numLetters exercise, establishing a connection & function for receiving and sending data
 
-app.post("/submit", (req, res) => {
-  const numLetters = req.body["fName"].length + req.body["lName"].length;
-  res.render("index.ejs", { numberOfLetters: numLetters });
+
+
+// blog routing
+app.get("/edit/:id", (req, res) => { // goes to webpage of the :id var
+    const postId = Number(req.params.id);
+    const postToEdit = posts.find((post) => post.id === postId);
+
+    if (!postToEdit) { // the webpage was crashing w/o this
+        return res.redirect("/");
+    }
+
+    res.render("index.ejs", {
+        posts: posts,
+        editPost: postToEdit
+    });
 });
+
+
+// blog stuff
+
+const posts = [];
+
+// create the post from data
+app.post("/submit", (req, res) => {
+    const newPost = {
+        id: Date.now(), // this makes a more complex id but it was easier
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content,
+        date: new Date().toDateString()
+    };
+
+    posts.push(newPost); //from 212 js lsn, to add to end of array
+
+    res.redirect("/"); // I hope this is ok, this'll just refresh the page to display the changes but I know we want versitality. 
+});
+
+/* sample
+[
+  {
+    id: 1,
+    title: "post",
+    author: "",
+    date: "",
+    content: "hi"
+  },
+  {
+    id: 2,
+    title: "post 2",
+    author: "",
+    date: "",
+    content: ""
+  }
+]
+*/
+
+
 
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
 
